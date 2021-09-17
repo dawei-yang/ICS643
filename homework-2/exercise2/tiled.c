e25e579e45aa8d99eae79b21a217ab93ac9d7338
Dyang <daweiyang@MacBook-Pro.home>
2021-09-16 19:59:22 -1000

    Q2 completed

--- homework-2/exercise2/tiled.c
+++ homework-2/exercise2/tiled.c
@@ -6,19 +6,24 @@
 struct timeval start, end;
 
 int main(int argc, char *argv[]) {
-    if(argc != 2) {
-        printf("Usage: ./nontiled <N>\n");
+    if(argc != 3) {
+        printf("Usage: ./tiled <array size> <tile size>\n");
         return -1;
     }
     if(isdigit(atoi(argv[1])) != 0 || atoi(argv[1]) <= 0) {
         printf("Invalid N argument \"%s\"\n", argv[1]);
         return -1;
     }
-    
+    if(isdigit(isdigit(atoi(argv[2]))) != 0  || atoi(argv[2]) <= 0 ) {
+        printf("Invalid B argument \"%s\"\n", argv[2]);
+        return -1;
+    }
     int N = atoi(argv[1]);
+    int B = atoi(argv[2]);
     double *arrayA = (double *)malloc(N * N * sizeof(double));
     double *arrayB = (double *)malloc(N * N * sizeof(double));
 
+    // initialize the Arrays
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < N; j++) {
             arrayA[N * i + j] = (double)(i + j);
@@ -26,21 +31,21 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // calculate
     gettimeofday(&start, NULL);
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            arrayA[N * i + j] += arrayB[N * i + j];
-            // arrayA[N * i + j] +=  arrayB[N * j + i];
-
+    for(int i=0; i<N; i+=B) {
+        for(int j=0; j<N; j+=B) {
+            for(int k=0; k<B; k++) {
+                for(int l=0; l<B; l++) {
+                    // arrayD[(N*i+j) + (k*N) + l] = arrayA[(N*i+j) + (k*N) + l]   + arrayB[(N*i+j) + (k*N) + l] ;
+                    arrayA[(N*i+j) + (k*N) + l] += arrayB[(N*i+j) + (k*N) + l];
+                }
+            }
         }
     }
     gettimeofday(&end, NULL);
+    
     double elapsed_time = (10.0E+6 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec) / 10.0E+6;
     printf("%lf\n", elapsed_time);
-
-    /* for(int i = 0; i < N*N; i++) {
-        printf("%lf\t", arrayA[i]);
-    } */
-
     return 0;
 }
