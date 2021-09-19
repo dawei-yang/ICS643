c083b11023173d2d5659cc81a2489cfa04c9e1d1
Dyang <daweiyang@MacBook-Pro.home>
2021-09-18 23:51:39 -1000

    work on mana

--- homework-2/exercise2/tiled_debug.c
+++ homework-2/exercise2/tiled_debug.c
@@ -45,8 +45,11 @@ int main(int argc, char *argv[]) {
     double *arrayA = (double *)malloc(N * N * sizeof(double));
     double *arrayB = (double *)malloc(N * N * sizeof(double));
 
+    double *arrayC = (double *)malloc(N * N * sizeof(double));
+    double *arrayD = (double *)malloc(N * N * sizeof(double));
+
     // initialize the Arrays
-    int i, j, k, l;
+    int i, j, k, l, p, q;
     for(i = 0; i < N; i++) {
         for(j = 0; j < N; j++) {
             arrayA[N * i + j] = (double)(i + j);
@@ -54,21 +57,55 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    for(i = 0; i < N; i++) {
+        for(j = 0; j < N; j++) {
+            arrayC[N * i + j] = arrayA[N * i + j] + arrayB[N * j + i];
+        }
+    }
+
     // calculate
     gettimeofday(&start, NULL);
+/*     for (i=0; i< N; i+=B) {
+        for (j=0; j < N; j+=B) {
+            for (p=i; p < i+B; p++) {
+                for (q=j; q < j+B; q++) {
+                    arrayD[B * p + q] = arrayA[B * p + q] + arrayB[B * q + p];
+                }
+            }
+        }
+    } */
+
 
     for(i=0; i<N; i+=(B>(N-i)? N%B : B)) {
         for(j=0; j<N; j+=(B>(N-j)? N%B: B)) {
             for(k=0; k<(B>(N-i)? N%B : B); k++) {
                 for(l=0; l<(B>(N-j)? N%B : B); l++) {
-                    arrayA[(N*i+j) + (k*N) + l] += arrayB[(N*j+i) + (l*N) + k];
+                    // arrayA[(N*i+j) + (k*N) + l] += arrayB[(N*j+i) + (l*N) + k];
+                    arrayD[(N*i+j) + (k*N) + l] = arrayA[(N*i+j) + (k*N) + l] += arrayB[(N*j+i) + (l*N) + k];
+
                 }
             }
         }
     }
     gettimeofday(&end, NULL);
+    double sumC = 0.0;
+    double sumD = 0.0;
+    for(i=0; i<N*N; i++) {
+        sumC += arrayC[i];
+        sumD += arrayD[i];
+    }
+
+    for(i = 0; i < N*N; i++) {
+        printf("%lf\t", arrayC[i]);
+    }
+    printf("\n");
+    for(i = 0; i < N*N; i++) {
+        printf("%lf\t", arrayD[i]);
+    }
 
     double elapsed_time = (10.0E+6 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec) / 10.0E+6;
     printf("%lf\n", elapsed_time);
+    printf("arrayC: %lf\n", sumC);
+    printf("arrayD: %lf\n", sumD);
     return 0;
 }
