fdbf59dbff81227aa4b0c86ed0a498a285a472ad
Dyang <daweiyang@MacBook-Pro.home>
2021-10-26 17:26:13 -1000

    remove send in both activity 1 and 2, sub_bcast works

--- homework-5/activity_2/matmul.c
+++ homework-5/activity_2/matmul.c
@@ -5,58 +5,8 @@
 #include <string.h>
 #include <math.h>
 
-// See for the (bad) default random number generator
-#define RAND_SEED 842270
-
-// Number of bytes to broadcast
-
-#define NUM_BYTES 100000000
-
-///////////////////////////////////////////////////////
-//// program_abort() and print_usage() functions //////
-///////////////////////////////////////////////////////
-
-static void program_abort(char *exec_name, char *message);
-static void print_usage();
-
-// Abort, printing the usage information only if the
-// first argument is non-NULL (and hopefully set to argv[0]), and
-// printing the second argument regardless.
-static void program_abort(char *exec_name, char *message) {
-  int my_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
-  if (my_rank == 0) {
-    if (message) {
-      fprintf(stderr,"%s",message);
-    }
-    if (exec_name) {
-      print_usage(exec_name);
-    }
-  }
-  MPI_Abort(MPI_COMM_WORLD, 1);
-  exit(1);
-}
-
-// Print the usage information
-static void print_usage(char *exec_name) {
-  int my_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
-
-  if (my_rank == 0) {
-    fprintf(stderr,"Usage: smpirun --cfg=smpi/bcast:mpich -np <num processes>\n");
-    fprintf(stderr,"              -platform <XML platform file> -hostfile <host file>\n");
-    fprintf(stderr,"              %s <bcast implementation name> [-c <chunk size>]\n",exec_name);
-    fprintf(stderr,"MPIRUN arguments:\n");
-    fprintf(stderr,"\t<num processes>: number of MPI processes\n");
-    fprintf(stderr,"\t<XML platform file>: a Simgrid platform description file\n");
-    fprintf(stderr,"\t<host file>: MPI host file with host names from the platform file\n");
-    fprintf(stderr,"PROGRAM arguments:\n");
-    fprintf(stderr,"\t<bcast implementation name>: the name of the broadcast implementaion (e.g., naive_bcast)\n");
-    fprintf(stderr,"\t[-c <chunk size>]: chunk size in bytes for message splitting (optional)\n");
-    fprintf(stderr,"\n");
-  }
-  return;
-}
+#define A(x,y) ARRAY_A[(x)*(TILESIZE)+(y)]
+#define B(x,y) ARRAY_B[(x)*(TILESIZE)+(y)]
 
 ///////////////////////////
 ////// Main function //////
@@ -65,95 +15,73 @@ static void print_usage(char *exec_name) {
 int main(int argc, char *argv[])
 {
 	int N;
-
-	// Parse command-line arguments (not using getopt because not thread-safe
-	// and annoying anyway). The code below ignores extraneous command-line
-	// arguments, which is lame, but we're not in the business of developing
-	// a cool thread-safe command-line argument parser.
-
 	MPI_Init(&argc, &argv);
 
-	// Bcast implementation name
 	if (argc < 2) {
-		program_abort(argv[0],"Missing <Dimension of Matrix N> argument\n");
+		printf("Missing <Dimension of Matrix N> argument\n");
 	} else {
 		N = strtol(argv[1], NULL, 10);
 	}
 
+	// For testing purpose
+	int buffer[1]; 
 	// Determine rank and number of processes
 	int num_procs;
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-	int buffer[2] = {0, 0};
-	int tile_size =  N/(sqrt(num_procs));
+	int TILESIZE =  (int)(N/(sqrt(num_procs)));
+	int row_num = (int)(rank/sqrt(num_procs));
+	int col_num = rank%(int)sqrt(num_procs);
+	int num_of_columns = (int)sqrt(num_procs);
+	// printf("rank [%d]\tComm_Row\tcolor [%d]\tkey[%d]\n", rank, row_num, col_num);
+	// printf("rank [%d]\tComm_Row\tcolor [%d]\tkey[%d]\n", rank, col_num+(int)sqrt(num_procs), row_num);
 
-	if (0 == rank) { 
-		fprintf(stderr,"N = [%d]\n",N);	
-		int dist = 1;
-		for (int i = 0; i < N; i+= tile_size) {
-			for (int j = 0; j < N; j+= tile_size) {
-				buffer[0] = i;
-				buffer[1] = j;
-				if( !(i == 0 && j == 0)) MPI_Send(&buffer, 2, MPI_INT, dist++, 0, MPI_COMM_WORLD);
-			}
+	double *ARRAY_A = (double *)malloc(TILESIZE*TILESIZE*sizeof(double));
+	double *ARRAY_B = (double *)malloc(TILESIZE*TILESIZE*sizeof(double));
+
+
+	for(int i=0; i<TILESIZE; i++) {
+		for(int j=0; j<TILESIZE; j++) {
+			A(i, j) = i + row_num*TILESIZE;
+			B(i, j) = i + row_num*TILESIZE + j+ col_num*TILESIZE;
 		}
-	}else {
-		MPI_Recv(&buffer, 2, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		double** A = (double**)malloc(tile_size*sizeof(double*));
-		double** B = (double**)malloc(tile_size*sizeof(double*));
-
-		for(int i=0; i< tile_size; i++) {
-			A[i] = (double*)malloc(tile_size*sizeof(double));
-			B[i] = (double*)malloc(tile_size*sizeof(double));
-		}
-		for(int i=0; i<tile_size; i++) {
-			for(int j=0; j<tile_size; j++) {
-				A[i][j] = i + buffer[0];
-			}
-		}
-		int global_i = buffer[0]/tile_size;
-		int global_j = buffer[1]/tile_size;
-
-/* 		for(int i=0; i<tile_size; i++) {
-			for(int j=0; j<tile_size; j++) {
-				printf("%.2lf\t", A[i][j]);
-			}
-			printf("\n");
-		} */
-		for(int i=0; i<tile_size; i++) {
-			for(int j=0; j<tile_size; j++) {
-				B[i][j] = i + buffer[0] + j + buffer[1];
-			}
-		}
-
-/* 		for(int i=0; i<tile_size; i++) {
-			for(int j=0; j<tile_size; j++) {
-				printf("%.2lf\t", B[i][j]);
-			}
-			printf("\n");
-		} */
-
 	}
 
+	MPI_Comm new_comm_row;
+	MPI_Comm new_comm_col;
 
-	if(rank == 0) {
-		MPI_Comm new_comm;
-		MPI_Comm_split(MPI_COMM_WORLD, 0, 0, &new_comm);
-		int new_rank;
-		MPI_Comm_rank(new_comm, &new_rank);
+	MPI_Comm_split(MPI_COMM_WORLD, row_num, col_num, &new_comm_row);
+	MPI_Comm_split(MPI_COMM_WORLD, col_num+num_of_columns, row_num, &new_comm_col);
+
+	if(rank == 0) buffer[0] = 1;
+	if(rank == 1) buffer[0] = 2;
+	if(rank == 2) buffer[0] = 3;
+
+	MPI_Bcast(buffer, 1, MPI_INT, 0, new_comm_col);
+	MPI_Bcast(buffer, 1, MPI_INT, 1, new_comm_col);
+	MPI_Bcast(buffer, 1, MPI_INT, 2, new_comm_col);
+
+	printf("rank [%d] buffer [%d]\n",rank, buffer[0]);
+	
+	// printf("rank %d has row comm with color [%d] and key [%d]\n", rank, row_num, col_num);
+	// printf("rank %d has col comm with color [%d] and key [%d]\n", rank, col_num+num_of_columns, row_num);
+
+	// test
+/* 	if(rank==0) {
+		buffer[0] = 43;
+		MPI_Bcast(buffer, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	}
-	else {
-		int global_i = buffer[0]/tile_size;
-		int global_j = buffer[1]/tile_size;
-		MPI_Comm new_comm;
-		MPI_Comm_split(MPI_COMM_WORLD, global_j, global_j, &new_comm);
-		int new_rank;
-		MPI_Comm_rank(new_comm, &new_rank);
-		printf("rank %d has new rank with color [%d] and key [%d]\n", rank, global_i, global_j);
-		printf("comm: %u\n", new_comm);
-	}
+	// if(rank==1)	MPI_Recv(buffer, 1, MPI_BYTE, 0, 0, new_comm_row, MPI_STATUS_IGNORE);
+	MPI_Barrier(MPI_COMM_WORLD);
+	printf("rank: %d\t buffer [%d]\n", rank, buffer[0]); */
+	
+	
+
+	
+	MPI_Comm_free(&new_comm_row);
+	MPI_Comm_free(&new_comm_col);
 
 	MPI_Finalize();
 	return 0;
