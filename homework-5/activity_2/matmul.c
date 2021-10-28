0944a82b61fd42d949b8cfb023c9e2aed736ee74
Dyang <daweiyang@MacBook-Pro.home>
2021-10-28 09:37:52 -1000

    broadcast works

--- homework-5/activity_2/matmul.c
+++ homework-5/activity_2/matmul.c
@@ -7,10 +7,10 @@
 
 #define A(x,y) ARRAY_A[(x)*(TILESIZE)+(y)]
 #define B(x,y) ARRAY_B[(x)*(TILESIZE)+(y)]
+#define C(x,y) ARRAY_C[(x)*(TILESIZE)+(y)]
+#define TA(x,y) TEMP_A[(x)*(TILESIZE)+(y)]
+#define TB(x,y) TEMP_B[(x)*(TILESIZE)+(y)]
 
-///////////////////////////
-////// Main function //////
-///////////////////////////
 
 int main(int argc, char *argv[])
 {
@@ -23,66 +23,71 @@ int main(int argc, char *argv[])
 		N = strtol(argv[1], NULL, 10);
 	}
 
-	// For testing purpose
-	int buffer[1]; 
 	// Determine rank and number of processes
 	int num_procs;
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-	int TILESIZE =  (int)(N/(sqrt(num_procs)));
-	int row_num = (int)(rank/sqrt(num_procs));
-	int col_num = rank%(int)sqrt(num_procs);
-	int num_of_columns = (int)sqrt(num_procs);
-	// printf("rank [%d]\tComm_Row\tcolor [%d]\tkey[%d]\n", rank, row_num, col_num);
-	// printf("rank [%d]\tComm_Row\tcolor [%d]\tkey[%d]\n", rank, col_num+(int)sqrt(num_procs), row_num);
+	int p = (int)sqrt(num_procs);
+	int TILESIZE =  (int)(N/p);
+	int row_num = (int)(rank/p);
+	int col_num = rank%p;
 
 	double *ARRAY_A = (double *)malloc(TILESIZE*TILESIZE*sizeof(double));
 	double *ARRAY_B = (double *)malloc(TILESIZE*TILESIZE*sizeof(double));
+	double *ARRAY_C = (double *)malloc(TILESIZE*TILESIZE*sizeof(double));
+	double *TEMP_A = (double *)malloc(TILESIZE*TILESIZE*sizeof(double));
+	double *TEMP_B = (double *)malloc(TILESIZE*TILESIZE*sizeof(double));
 
-
+	// initialize
 	for(int i=0; i<TILESIZE; i++) {
 		for(int j=0; j<TILESIZE; j++) {
-			A(i, j) = i + row_num*TILESIZE;
-			B(i, j) = i + row_num*TILESIZE + j+ col_num*TILESIZE;
+			A(i, j) = i+row_num*TILESIZE;
+			B(i, j) = i+row_num*TILESIZE+j+col_num*TILESIZE;
 		}
 	}
 
+	// Establish row and col sub communicators
 	MPI_Comm new_comm_row;
 	MPI_Comm new_comm_col;
-
 	MPI_Comm_split(MPI_COMM_WORLD, row_num, col_num, &new_comm_row);
-	MPI_Comm_split(MPI_COMM_WORLD, col_num+num_of_columns, row_num, &new_comm_col);
+	MPI_Comm_split(MPI_COMM_WORLD, col_num+p, row_num, &new_comm_col);
 
-	if(rank == 0) buffer[0] = 1;
-	if(rank == 1) buffer[0] = 2;
-	if(rank == 2) buffer[0] = 3;
+	for(int k = 0; k<=p-1; k++) {
+		// copy to temp 
+		for(int i=0; i<TILESIZE; i++) {
+			for(int j=0; j<TILESIZE; j++) {
+				TA(i, j) = A(i, j);
+				TB(i, j) = B(i, j);
+			}
+		}
 
-	MPI_Bcast(buffer, 1, MPI_INT, 0, new_comm_col);
-	MPI_Bcast(buffer, 1, MPI_INT, 1, new_comm_col);
-	MPI_Bcast(buffer, 1, MPI_INT, 2, new_comm_col);
+		// Boardcast A_?k OR B_k?
+		MPI_Bcast(TEMP_A, TILESIZE*TILESIZE, MPI_DOUBLE, k, new_comm_row);
+		MPI_Bcast(TEMP_B, TILESIZE*TILESIZE, MPI_DOUBLE, k, new_comm_col);
+		MPI_Barrier(MPI_COMM_WORLD);
 
-	printf("rank [%d] buffer [%d]\n",rank, buffer[0]);
-	
-	// printf("rank %d has row comm with color [%d] and key [%d]\n", rank, row_num, col_num);
-	// printf("rank %d has col comm with color [%d] and key [%d]\n", rank, col_num+num_of_columns, row_num);
-
-	// test
-/* 	if(rank==0) {
-		buffer[0] = 43;
-		MPI_Bcast(buffer, 1, MPI_INT, 0, MPI_COMM_WORLD);
+		// calculate C
+		for(int i=0; i<TILESIZE; i++) {
+			for(int j=0; j<TILESIZE; j++) {
+				// printf("rank [%d]\t TA = %lf; TB = %lf\n", rank, TA(i, j), TB(i, j));
+				C(i, j) += TA(i, j)*TB(i, j);
+			}
+		}
 	}
-	// if(rank==1)	MPI_Recv(buffer, 1, MPI_BYTE, 0, 0, new_comm_row, MPI_STATUS_IGNORE);
-	MPI_Barrier(MPI_COMM_WORLD);
-	printf("rank: %d\t buffer [%d]\n", rank, buffer[0]); */
-	
-	
 
-	
+	// calculate sum of C for evaluation
+	double sum = 0;
+	for(int i=0; i<TILESIZE; i++) {
+		for(int j=0; j<TILESIZE; j++) {
+			sum += C(i, j);
+		}
+	}
+	printf("rank [%d] sum = %lf\n", rank, sum);
+
 	MPI_Comm_free(&new_comm_row);
 	MPI_Comm_free(&new_comm_col);
-
 	MPI_Finalize();
 	return 0;
 }
